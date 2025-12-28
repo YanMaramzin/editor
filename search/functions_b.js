cd23544168b3a93550b072772168fab87a1dd0bf
YanMaramzin <YanMaramzin@users.noreply.github.com>
2025-12-28 21:07:24 +0000

    deploy: 8df7f7f30e215f2ab390649db434ee97bf69bb2b

--- search/functions_b.js
+++ search/functions_b.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['_7egraphicobject_0',['~GraphicObject',['../classGraphicObject.html#ac846d87c44dfa5784d44073542bea8c5',1,'GraphicObject']]],
-  ['_7eview_1',['~View',['../classView.html#ad0f3ade04bca4e74b8868e63729bad86',1,'View']]]
+  ['_7ecircle_0',['~Circle',['../classCircle.html#af7a52228c8211080cf2100b55c8a7a64',1,'Circle']]],
+  ['_7egraphicobject_1',['~GraphicObject',['../classGraphicObject.html#ac846d87c44dfa5784d44073542bea8c5',1,'GraphicObject']]],
+  ['_7eview_2',['~View',['../classView.html#ad0f3ade04bca4e74b8868e63729bad86',1,'View']]]
 ];
